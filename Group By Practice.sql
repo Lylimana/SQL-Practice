088d9ced0427478c1afb5be941f507252e45e0b1
Miguel Manalili <manalili.mig@gmail.com>
2025-08-22 00:23:42 +0100

    Group By Practice Continued

--- Group By Practice.sql
+++ Group By Practice.sql
@@ -1,6 +1,51 @@
 # GROUP BY 
+SELECT *
+FROM employee_demographics;
+
 
 SELECT gender
-FROM employee _demographics
-GROUP BY Gender
+FROM employee_demographics
+GROUP BY gender
+; 
+
+SELECT gender, AVG(age)
+FROM employee_demographics
+GROUP BY gender
+; 
+
+SELECT gender, AVG(age), MIN(age) , MAX(age), COUNT(age)
+FROM employee_demographics
+GROUP BY gender
+; 
+
+SELECT occupation, AVG(salary), MAX(salary), MIN(salary)
+FROM employee_salary
+GROUP BY occupation 
+;
+
+# ORDER BY 
+SELECT *
+FROM employee_demographics
+ORDER BY age 
+; 
+
+SELECT *
+FROM employee_demographics
+ORDER BY birth_date ASC
+; 
+
+SELECT *
+FROM employee_demographics
+ORDER BY last_name DESC
+; 
+
+# Order matters...
+SELECT *
+FROM employee_demographics
+ORDER BY gender DESC, age ASC
+; 
+
+SELECT *
+FROM employee_demographics
+ORDER BY 2
 ; 
